163a4789eee9e688cd2f7b0c87923c150d2cdb4d
Sajer24 <sajer.kanat1992@gmail.com>
2022-09-07 00:45:46 +0300

    Домашнее задание номер 2 готово

---
+++ src/Zadanye1_3.java
@@ -0,0 +1,19 @@
+public class Zadanye1_3 {
+    public static void main(String[] args) {
+        var dog = 8;
+        var cat =3.6;
+        var paper = 763789;
+        System.out.println(dog+ " "+ cat+ " "+ paper);
+
+        var dog1 = dog+4;
+        var cat1= cat+4;
+        var paper1=paper+ 4;
+        System.out.println(dog1+ " "+ cat1+ " "+ paper1);
+
+        var dog2 = dog1-3.5;
+        var cat2= cat1-1.6;
+        var paper2=paper1- 7639;
+        System.out.println(dog2+ " "+ cat2+ " "+ paper2);
+
+    }
+}
---
+++ src/zadanye6_8.java
@@ -0,0 +1,24 @@
+public class zadanye6_8 {
+    public static void main(String[] args) {
+        var boxer1Weight = 78.2;
+        var boxer2Weight = 82.7;
+        var sum = boxer2Weight + boxer1Weight;
+        System.out.println(sum);
+        var difference = boxer2Weight%boxer1Weight;
+        System.out.println(difference);
+        var difference2= boxer2Weight-boxer1Weight;
+        System.out.println(difference2);
+
+
+        var summa = 640;
+        var one = 8;
+        var employee= summa/one;
+        System.out.println("Всего работников в компании – "+ employee + " человек");
+
+        var max = employee + 94;
+        var result = max* one;
+        System.out.println("Если в компании работает "+ max + " человек, то всего "+ result+ " часов работы может быть поделено между сотрудниками");
+
+
+    }
+}
